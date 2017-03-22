aedecdba4565f5da35f84f9002aeaa27ec2a9e9e
Emil Blomquist <mr.emil.blomquist@gmail.com>
2017-03-22 23:19:26 +0100

    save

--- dispersion.py
+++ dispersion.py
@@ -2,6 +2,7 @@ import glob
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.signal as sig
+from scipy.odr import *
 
 import numbers
 
@@ -9,8 +10,8 @@ import warnings
 warnings.filterwarnings(action="ignore", module="scipy", message="^internal gelsd")
 warnings.filterwarnings(action="ignore", module="matplotlib", message="^tight_layout")
 
-fileNames = ['a=1 mu=-0.02/*']
-# fileNames = ['./p=0/*']
+fileNames = ['a=1 N=30B t=80/*', 'a=1 N=30B t=50/*', 'a=1 N=50B/*']
+# fileNames = ['a=1 N=50B/*']
 for fileName in fileNames:
   paths = glob.glob('./data/' + fileName + '.txt', recursive=True)
 
@@ -32,6 +33,8 @@ for fileName in fileNames:
   Es = []
   Zs = []
   As = []
+  Es_err = []
+  Zs_err = []
 
 
   for j, path in enumerate(paths):
@@ -108,48 +111,69 @@ for fileName in fileNames:
 
         logG = sig.savgol_filter(np.log(g), 51, 3)
         diffLogG = sig.savgol_filter(np.diff(logG), 51, 3)/(2*T[0])
-        diff2LogG = sig.savgol_filter(np.diff(diffLogG), 51, 3)/(2*T[0])
 
-        lowerBound = np.argmax(diff2LogG < diff2LogG[0]*0.0000001)
-        lowerBound = 250*3
+        # lower bound
+        lowerBound = 0
+        interval = 200
+        for i, val in enumerate(diffLogG):
+          found = True
+          if i + interval <= diffLogG.size - 1:
+            for j in range(i + 1, i + 1 + interval):
+              if diffLogG[i] < diffLogG[j]:
+                found = False
 
+          if found:
+            lowerBound = i
+            break
 
-        data = np.diff(diffLogG[lowerBound:])/(2*T[0])
-        mean = np.mean(data)
-        deviation = np.abs(data - mean)
-        deviation = deviation/np.sum(deviation)
-        deviation = deviation - deviation[0]
-        maxmax = 0.01
-        # maxmax = np.mean(deviation)
-        upperBound = np.argmax(deviation > maxmax) - 1 #makeFinite - lowerBound - 3
-        if upperBound == -1:
-          upperBound = makeFinite - lowerBound - 3
-
-        M1 = T[lowerBound:lowerBound+upperBound].reshape((T[lowerBound:lowerBound+upperBound].size, 1))
-        M2 = np.ones((T[lowerBound:lowerBound+upperBound].size, 1))
-        M = np.concatenate((M1, M2), axis=1)
-        Y = np.log(G[lowerBound:lowerBound+upperBound]).reshape((G[lowerBound:lowerBound+upperBound].size, 1))
-
-        k, m = tuple(np.linalg.inv(M.T.dot(M)).dot(M.T.dot(Y)))
+        
+        upperBound = makeFinite - lowerBound - 250
 
 
-        E0 = parameters['mu'] - k[0]
-        Z0 = np.exp(m[0])
+
+
+
+
+        # Define a function (quadratic in our case) to fit the data with.
+        def exp_fit(params, t):
+          Z, E = params
+          return Z*np.exp(-(E - parameters['mu'])*t)
+
+        # Create a model for fitting.
+        exp_model = Model(exp_fit)
+
+        # Create a RealData object using our initiated data from above.
+        data = RealData(T[lowerBound:lowerBound+upperBound], G[lowerBound:lowerBound+upperBound])
+
+        # Set up ODR with the model and data.
+        odr = ODR(data, exp_model, beta0=[1, 0])
+
+        # Run the regression.
+        out = odr.run()
+
+        Z = out.beta[0]
+        E = out.beta[1]
+
+        Z_err = out.sd_beta[0]
+        E_err = out.sd_beta[1]
+
 
         Ps.append(parameters['p'])
-        Es.append(E0)
-        Zs.append(Z0)
+        Es.append(E)
+        Zs.append(Z)
         As.append(parameters['a'])
+        Es_err.append(E_err)
+        Zs_err.append(Z_err)
 
         print('-------')
         print('a=' + str(parameters['a']))
         print('p=' + str(parameters['p']))
-        print('E0=' + str(E0))
-        print('Z0=' + str(Z0))
+        print('E=' + str(E))
+        print('Z=' + str(Z))
 
 
         if False:
-          f, axarr = plt.subplots(5, sharex=True, figsize=(20, 10))
+          f, axarr = plt.subplots(3, sharex=True, figsize=(20, 10))
 
           axarr[0].set_title(
             r'$p = {0}, \, \alpha = {1}, \, \mu = {2}, \, N = {3} \, E = {4}, \, Z = {5}$'.format(
@@ -157,14 +181,14 @@ for fileName in fileNames:
               parameters['a'],
               parameters['mu'],
               parameters['N'],
-              E0,
-              Z0
+              E,
+              Z
             )
           )
 
           axarr[0].plot(T, G)
           axarr[0].plot(T[lowerBound], G[lowerBound], 'o', color='green')
-          axarr[0].plot(T, Z0*np.exp(-(E0 - parameters['mu'])*T), '--', color='#ff00ee')
+          axarr[0].plot(T, Z*np.exp(-(E - parameters['mu'])*T), '--', color='#ff00ee')
           axarr[0].plot(T[upperBound + lowerBound], G[upperBound + lowerBound], 'o', color='green')
 
 
@@ -172,27 +196,14 @@ for fileName in fileNames:
           axarr[1].plot(t, logG, '--', color='orange')
           axarr[1].plot(T[lowerBound], logG[lowerBound], 'o', color='green')
           axarr[1].plot(T[upperBound + lowerBound], logG[upperBound + lowerBound], 'o', color='green')
-          axarr[1].plot(T, m + k*T, '--', color='#ff00ee')
+          axarr[1].plot(T, np.log(Z) - (E - parameters['mu'])*T, '--', color='#ff00ee')
 
 
           axarr[2].plot(t[0: -1], np.diff(logG, n=1)/(2*T[0]))
           axarr[2].plot(t[0: -1], diffLogG, 'orange')
           axarr[2].plot(T[lowerBound], diffLogG[lowerBound], 'o', color='green')
           axarr[2].plot(T[upperBound + lowerBound], diffLogG[upperBound + lowerBound], 'o', color='green')
-          axarr[2].plot(T, k + T*0, '--', color='#ff00ee')
-
-
-          axarr[3].plot(t[0: -2], np.diff(diffLogG)/(2*T[0]))
-          axarr[3].plot(t[0: -2], diff2LogG, 'orange')
-          axarr[3].plot(T[lowerBound], diff2LogG[lowerBound], 'o', color='green')
-          axarr[3].plot(T[upperBound + lowerBound], diff2LogG[upperBound + lowerBound], 'o', color='green')
-
-
-          axarr[4].plot(t[lowerBound: -2], deviation)
-          axarr[4].plot([0, t[-1]], [maxmax] * 2, '--', color='red')
-          axarr[4].plot([0, t[-1]], [0] * 2, '--', color='red')
-          axarr[4].plot(T[lowerBound], deviation[0], 'o', color='green')
-          axarr[4].plot(T[upperBound + lowerBound], deviation[upperBound], 'o', color='green')
+          axarr[2].plot(T, - (E - parameters['mu']) + T*0, '--', color='#ff00ee')
 
           plt.tight_layout()
           plt.show()
@@ -202,21 +213,22 @@ for fileName in fileNames:
 
   # plt.plot(Ps, Es)
 
+  plt.plot(Ps, Es, '-')
+  plt.errorbar(Ps, Es, yerr=Es_err, linestyle='None', marker='x')
+
+
 x = np.array([0, 20.657, 32.947, 46.544, 65.981, 92.46, 104, 131, 147, 161, 168, 174, 180, 186, 189, 192])/104
 y = np.array([182, 180, 176, 168.9, 154, 125.8, 111, 72.3, 48.7, 30.3, 21.31, 13.23, 7.334, 3.234, 2.109, 1.5])/(-181)
 
-plt.plot(x, y)
-plt.plot(x, np.array(y) - 0.05)
-
-print(x)
-print(np.array(y) - 0.05)
-
-# plt.show()
+plt.plot(x, y, ':')
 
 
 #   plt.plot(Ps, Zs)
-plt.plot(Ps, Es, '-o')
-plt.plot(Ps, Es, '-o')
 
+# print(Es_err)
+
+plt.errorbar(Ps, Es, yerr=Es_err, linestyle='None', marker='x')
+
+plt.savefig('plots/foo.pdf')
 plt.show()
 
